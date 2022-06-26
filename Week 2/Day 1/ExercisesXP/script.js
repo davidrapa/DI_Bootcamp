12313e3c5b44a2788d95ddfe6c9f46be95b6be72
davidrapa <davidrapaport01@gmail.com>
2022-06-26 13:05:43 +0300

    week2

---
+++ Week 2/Day 1/ExercisesXP/script.js
@@ -0,0 +1,7 @@
+
+
+let userName = "Leana";
+console.log ("userName");
+
+let ageOfUser;
+ageOfUser=32
---
+++ Week 2/Day 1/script.js
@@ -0,0 +1,4 @@
+let pets= ['cat', 'dog', 'fish', 'rabbit' , 'cow']; 
+console.log(pets [1]);
+pets[3]='horse';
+console.log(pets);
