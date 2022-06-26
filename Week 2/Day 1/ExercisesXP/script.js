8a3b3ffaeb178aa86a28cfe0833c5347e8ea299c
davidrapa <davidrapaport01@gmail.com>
2022-06-26 14:21:13 +0300

    week2

--- Week 2/Day 1/ExercisesXP/script.js
+++ Week 2/Day 1/ExercisesXP/script.js
@@ -1,7 +1,24 @@
+let favoriteFood='chocolate';
+let favoriteMeal='dinner';
+//console.log('I eat '+ favoriteFood + ' at every '+ favoriteMeal);
+
+let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+let myWatchedSeriesLength = myWatchedSeries;  
+//console.log(myWatchedSeriesLength);
+
+let myWatchedSeriesSentence = myWatchedSeriesLength
+//console.log('I watched 3 series:'+ myWatchedSeriesSentence);
+
+myWatchedSeries.splice(2, 1, 'friends');
+//console.log(myWatchedSeries)
+
+myWatchedSeries.push('breaking bad')
+//console.log(myWatchedSeries)
+
+myWatchedSeries.splice(0, 1);
+//console.log(myWatchedSeries);
 
 
-let userName = "Leana";
-console.log ("userName");
+console.log(myWatchedSeries[1[2]])
 
-let ageOfUser;
-ageOfUser=32
+
--- Week 2/Day 1/script.js
+++
@@ -1,4 +0,0 @@
-let pets= ['cat', 'dog', 'fish', 'rabbit' , 'cow']; 
-console.log(pets [1]);
-pets[3]='horse';
-console.log(pets);
