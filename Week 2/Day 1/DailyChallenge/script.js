4c690a57de574ae529ddd14d195e34283ed6653a
davidrapa <davidrapaport01@gmail.com>
2022-06-27 07:13:21 +0300

    dailycW2D1

---
+++ Week 2/Day 1/DailyChallenge/script.js
@@ -0,0 +1,20 @@
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+fruits.splice(0, 1,);
+console.log(fruits);
+
+fruits.sort();
+console.log(fruits);
+
+fruits.push('kiwi')
+console.log(fruits);
+
+fruits.splice(0, 1,);
+console.log(fruits);
+
+fruits.reverse();
+console.log(fruits);
+
+//Excercise 2
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+console.log(moreFruits[1][1][0]);
+
