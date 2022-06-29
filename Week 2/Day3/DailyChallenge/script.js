32b298742cb6c50564f7f0a1d42a72f48c2fed7b
davidrapa <davidrapaport01@gmail.com>
2022-06-29 08:06:52 +0300

    ExcXP-W2D3DailyC

---
+++ Week 2/Day3/DailyChallenge/script.js
@@ -0,0 +1,14 @@
+for (let i = 1; i < 7; i++){
+    let star='';
+    star += '*';
+    console.log(star.repeat(i));
+
+}
+let star='*';
+for (let i = 1; i < 7; i++){
+    let star='';
+    for (let  = 0; j < 7 ; j++){
+        star += '*';
+    }
+        console.log(star);
+        }
